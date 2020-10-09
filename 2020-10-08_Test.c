ba24fa86e1ab3d69bfd00152b5c55a2cf8941326
Dylan-M-H <dmhosmer@outlook.com>
2020-10-08 22:12:34 -0500

    Initial Commmit

---
+++ 2020-10-08_Test.c
@@ -0,0 +1,14 @@
+/*
+2020-10-08_Test.c
+Author: Dylan M. Hosmer
+Created On: 08OCT20
+Last Modified: 08OCT20
+*/
+
+#include <stdio.h>
+
+
+int main() {
+   printf("\n2020-10-08_Test is alive!\n\n");
+   return 0;
+}
